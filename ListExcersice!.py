124be225791e4930e02195688a3bdff91dbc4035
GilbCruz13 <gilbcruz13@gmail.com>
2022-09-21 10:57:54 -0400

    Create ListExcersice!.py

---
+++ ListExcersice!.py
@@ -0,0 +1,14 @@
+#Take a list, say for example this one:
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#and write a program that prints out all the elements of the list that are less than 5.
+
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for x in a:
+    if x < 6:
+        print(x)
+
+#For loop and created an if statement, that prints all that numbers on the list that are less than 6
+# Output : 1,1,2,3,5 
